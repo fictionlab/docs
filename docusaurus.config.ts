8e5769d749d74ef792c0f48e083b6011caaf274d
Błażej Sowa <bsowa123@gmail.com>
2023-11-21 16:01:55 +0100

    Move to custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: undefined,
 
   // Set the production url of your site here
-  url: 'https://fictionlab.github.io',
+  url: 'https://docs.fictionlab.pl',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
