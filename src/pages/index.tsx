03eafc9d5354a6b1da449f050c1be5a0e2ba333f
Adrian Krzemiński <adrian@fictionlab.pl>
2025-02-24 11:20:08 +0100

    chore: Update Docusaurus to 3.7.0 (#36)
    * Add script for updating docusaurus
    
    * Bump docusaurus version
    
    * Add readme section for updating docusaurus
    
    * fix JSX import in React 19
    
    * Fix formatting
    
    * Comment out broken anchors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Link from '@docusaurus/Link';
 import ThemedImage from '@theme/ThemedImage';
 
 import styles from './index.module.css';
+import { JSX } from 'react';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
