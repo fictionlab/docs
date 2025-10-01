29a02fdf04d3c8c2c36e43800b05cb6e7a8818c4
Adrian Krzeminski <adrian@fictionlab.pl>
2025-10-01 16:05:18 +0200

    Fix props in FlexTable components

--- src/components/FlexTableItem/index.tsx
+++ src/components/FlexTableItem/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import styles from './styles.module.css';
+import clsx from 'clsx';
 
 export interface DivProps extends React.HTMLAttributes<HTMLDivElement> {
   mobileColumns?: 1 | 2;
 }
 
-export default function FlexTableItem({
-  children,
-  style,
-  mobileColumns,
-}: DivProps) {
-  let className;
-  if (mobileColumns != 2) className = styles.flexTableItem1;
-  else className = styles.flexTableItem2;
+export default function FlexTableItem(props: DivProps) {
+  const { children, mobileColumns, className, ...propsRest } = props;
+  let itemClassName;
+  if (mobileColumns != 2) itemClassName = styles.flexTableItem1;
+  else itemClassName = styles.flexTableItem2;
   return (
-    <div className={className} style={style}>
+    <div className={clsx(className, itemClassName)} {...propsRest}>
       {children}
     </div>
   );
