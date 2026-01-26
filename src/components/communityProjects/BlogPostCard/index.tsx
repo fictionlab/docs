e700a4dc15bbfe3045c78cc6afe024672e6687d7
Adrian Krzeminski <adrian@fictionlab.pl>
2026-01-26 14:16:45 +0100

    Card layout adjustments

--- src/components/communityProjects/BlogPostCard/index.tsx
+++ src/components/communityProjects/BlogPostCard/index.tsx
@@ -1,13 +1,14 @@
-import React, {type ReactNode} from 'react';
+import { type ReactNode } from 'react';
 import clsx from 'clsx';
-import {useBlogPost} from '@docusaurus/plugin-content-blog/client';
+import { useBlogPost } from '@docusaurus/plugin-content-blog/client';
 import BlogPostItemContainer from '@theme/BlogPostItem/Container';
-import type {Props} from '@theme/BlogPostItem';
+import type { Props } from '@theme/BlogPostItem';
 import styles from './styles.module.css';
 import Link from '@docusaurus/Link';
 import { PropBlogPostMetadata } from '@docusaurus/plugin-content-blog';
 
-interface communityProjectMetadata extends Omit<PropBlogPostMetadata, 'frontMatter'> {
+interface communityProjectMetadata
+  extends Omit<PropBlogPostMetadata, 'frontMatter'> {
   frontMatter: PropBlogPostMetadata['frontMatter'] & {
     company?: {
       name?: string;
@@ -17,13 +18,14 @@ interface communityProjectMetadata extends Omit<PropBlogPostMetadata, 'frontMatt
   };
 }
 
-export default function BlogPostCard({children, className}: Props): ReactNode {
-  const {metadata} = useBlogPost() as {metadata: communityProjectMetadata};
+export default function BlogPostCard({
+  children,
+  className,
+}: Props): ReactNode {
+  const { metadata } = useBlogPost() as { metadata: communityProjectMetadata };
 
   return (
-    <BlogPostItemContainer
-      className={clsx(className, styles.compactCard)}
-    >
+    <BlogPostItemContainer className={clsx(className, styles.compactCard)}>
       <Link to={metadata.permalink} className={styles.cardLink}>
         {metadata.frontMatter.image && (
           <div className={styles.cardImage}>
@@ -35,6 +37,21 @@ export default function BlogPostCard({children, className}: Props): ReactNode {
           {metadata.description && (
             <p className={styles.cardDescription}>{children}</p>
           )}
+          <div>
+            {metadata.frontMatter.company &&
+              metadata.frontMatter.company.name &&
+              (metadata.frontMatter.company.logo_url ? (
+                <img
+                  src={metadata.frontMatter.company.logo_url}
+                  alt={metadata.frontMatter.company.name}
+                  className={styles.cardCompanyLogo}
+                />
+              ) : (
+                <span className={styles.cardCompanyName}>
+                  {metadata.frontMatter.company.name}
+                </span>
+              ))}
+          </div>
           {metadata.tags && metadata.tags.length > 0 && (
             <div className={styles.cardTags}>
               {metadata.tags.slice(0, 3).map((tag) => (
@@ -44,25 +61,6 @@ export default function BlogPostCard({children, className}: Props): ReactNode {
               ))}
             </div>
           )}
-          <div className={styles.cardCompanyContainer}>
-            { metadata.frontMatter.company && metadata.frontMatter.company.name && metadata.frontMatter.company.url && (
-              <a
-                href={metadata.frontMatter.company.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={styles.cardCompany}
-              >
-                {metadata.frontMatter.company.logo_url && (
-                  <img
-                    src={metadata.frontMatter.company.logo_url}
-                    alt={metadata.frontMatter.company.name}
-                    className={styles.cardCompanyLogo}
-                  />
-                )}
-                <span className={styles.cardCompanyName}>{metadata.frontMatter.company.name}</span>
-              </a>
-            )}
-          </div>
         </div>
       </Link>
     </BlogPostItemContainer>
