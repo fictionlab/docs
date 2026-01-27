5edb73823a4660624483d1bfad7d9f7bfc188e9e
Adrian Krzeminski <adrian@fictionlab.pl>
2026-01-27 13:33:17 +0100

    Add hook for getting community project metadata

---
+++ src/hooks/useCommunityProject/index.tsx
@@ -0,0 +1,21 @@
+import { PropBlogPostMetadata } from '@docusaurus/plugin-content-blog';
+import { useBlogPost } from '@docusaurus/plugin-content-blog/lib/client/contexts.js';
+
+interface communityProjectMetadata
+  extends Omit<PropBlogPostMetadata, 'frontMatter'> {
+  frontMatter: PropBlogPostMetadata['frontMatter'] & {
+    company?: {
+      name?: string;
+      logo_url?: string;
+      url?: string;
+    };
+  };
+}
+
+export default function useCommunityProject() {
+  const blogPost = useBlogPost();
+  return {
+    ...blogPost,
+    metadata: blogPost.metadata as communityProjectMetadata
+  };
+}
